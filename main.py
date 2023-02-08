bdfad9bd638886e8b94791fb64daa90cf14934b8
donutnel <122060160+donutnel@users.noreply.github.com>
2023-02-08 15:47:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def start(update, context):
 
 def airdrop(update, context):
     user_id = update.message.from_user.id
-    channel_username = "YOUR_CHANNEL_USERNAME"
+    channel_username = "nostavid"
     try:
         if context.bot.get_chat_member(channel_username, user_id).status in ["left", "kicked"]:
             update.message.reply_text("Vous n'êtes pas abonné à notre canal. Veuillez vous abonner pour recevoir votre récompense en jetons TRON.")
@@ -28,7 +28,7 @@ def error(update, context):
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
 def main():
-    token = "YOUR_TELEGRAM_BOT_TOKEN"
+    token = "5852766319:AAFJLw-NKobFaE2Qq12CDRxeAuu000jZTjE"
 
     updater = Updater(token, use_context=True)
 
